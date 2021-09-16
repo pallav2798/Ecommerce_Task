54aaafa5371ee0572a128c4fb4d4ee9ed8ce04ff
pallav2798 <30664064+pallav2798@users.noreply.github.com>
2021-09-15 23:33:14 -0700

    Delete venv directory

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\PALLAV\PycharmProjects\Ecommerce_task\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
